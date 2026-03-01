8cb17201a0c08dfdd09e0671d4e804a0c569a574
Mark Langen <stravant@gmail.com>
2026-02-28 19:33:00 -0800

    Start TriangleMesh fresh

--- src/TriangleMesh.lua
+++ src/TriangleMesh.lua
@@ -2,40 +2,61 @@
 
 local fillTriangle = require("./fillTriangle")
 
+export type VertexId = number
+export type TriangleId = number
+export type EdgeId = number
+
+-- Use Vector3 as a hash key for vertex positions, though this more granular
+export type VertexHash = Vector3
+export type EdgeHash = Vector3
+
 export type Vertex = {
-	id: number,
+	id: VertexId,
 	position: Vector3,
-	triangles: { number }, -- triangle ids
+	triangles: { TriangleId },
 }
 
 export type Triangle = {
-	id: number,
+	id: TriangleId,
+
+	-- Intrusive linked list of triangles associated with a particular part. Usually we
+	-- only care about a single triangle per part and these will be empty. For
+	-- a box part or a part we've explored both sides of there may be two or more.
+	next: Triangle?,
+	prev: Triangle?,
+
+	-- Ordered such that (2 - 1) cross (3 - 2) is the outward normal
+	vertices: { VertexId },
+	normal: Vector3, -- Cached info, could be derived from vertices
+
+	-- The parts making up this triangle
+	parts: { BasePart },
 	thickness: number,
-	vertices: { number }, -- 3 vertex ids
-	parts: { BasePart }, -- 1-2 wedge parts
-	normal: Vector3,
-	invertedNormal: boolean, -- Does normal point the direction of (1->2) cross (2->3)?
+	partsRequireUpgrade: boolean?, -- Actually a Block part that needs to be converted to Wedge parts
 }
 
 export type Edge = {
-	key: string,
-	v1: number,
-	v2: number,
-	triangles: { number }, -- triangle ids sharing this edge
+	id: EdgeId,
+
+	triangles: { TriangleId },
+
+	-- Order is not important
+	v1: VertexId,
+	v2: VertexId,
 }
 
 export type TriangleMesh = {
 	-- Data access
-	getVertices: () -> { [number]: Vertex },
-	getTriangles: () -> { [number]: Triangle },
+	getVertices: () -> { [VertexId]: Vertex },
+	getTriangles: () -> { [TriangleId]: Triangle },
 	getEdges: () -> { [string]: Edge },
-	getVertex: (id: number) -> Vertex?,
-	getTriangle: (id: number) -> Triangle?,
+	getVertex: (id: VertexId) -> Vertex?,
+	getTriangle: (id: TriangleId) -> Triangle?,
 
 	-- Queries
 	getBoundaryEdges: () -> { Edge },
-	getVertexNeighbors: (vertexId: number) -> { number },
-	findVertexNear: (position: Vector3, radius: number) -> number?,
+	getVertexNeighbors: (vertexId: VertexId) -> { VertexId },
+	findVertexNear: (position: Vector3, radius: number) -> VertexId?,
 
 	-- Mutations
 	addTriangle: (v1Pos: Vector3, v2Pos: Vector3, v3Pos: Vector3, thickness: number, parent: Instance, props: fillTriangle.TriangleProps?, hintPoint: Vector3) -> number?,
@@ -44,21 +65,44 @@ export type TriangleMesh = {
 	moveVertices: (moves: { [number]: Vector3 }, thickness: number, props: fillTriangle.TriangleProps?) -> (),
 
 	-- Queries (topology)
-	getAdjacentTriangles: (triangleId: number) -> { number },
-	findTrianglesInRadius: (center: Vector3, radius: number) -> { number },
-	walkSurface: (seedTriangleId: number, center: Vector3, radius: number) -> ({ number }, { number }),
+	getAdjacentTriangles: (triangleId: TriangleId) -> { TriangleId },
+	findTrianglesInRadius: (center: Vector3, radius: number) -> { TriangleId },
+	walkSurface: (seedTriangleId: TriangleId, center: Vector3, radius: number) -> ({ TriangleId }, { VertexId }),
 
 	-- Discovery / Scanning
 	discoverPart: (part: BasePart, hintPoint: Vector3) -> number?,
-	discoverRegion: (seeds: { Vector3 }, radius: number) -> ({ number }, { number }),
+	discoverRegion: (seeds: { Vector3 }, radius: number) -> { TriangleId },
 	getPartTriangle: (part: BasePart, hintPoint: Vector3) -> number?,
-	getPartTriangles: (part: BasePart) -> { number },
+	getPartTriangles: (part: BasePart) -> { TriangleId },
 	clear: () -> (),
 }
 
+local function hashVertex(position: Vector3): VertexHash
+	local asVector = (position :: any) :: vector
+	local result = (vector.floor((asVector / 0.01) + vector.one * 0.513456))
+	return (result :: any) :: VertexHash
+end
+
+-- Order is not important, hashEdge(a, b) == hashEdge(b, a)
+local function hashEdge(v1: Vector3, v2: Vector3): EdgeHash
+	return hashVertex(v1) + hashVertex(v2)
+end
+
 local function createTriangleMesh(): TriangleMesh
-	-- TODO: Re-implement with better internals
-	error("Not yet implemented")
+	local mTriangles = {} :: {[TriangleId]: Triangle}
+	local mVertices = {} :: {[VertexId]: Vertex}
+	local mEdges = {} :: {[EdgeId]: Edge}
+
+	-- Head of linked list of Triangles for a given part
+	local mPartToTriangles = {} :: {[BasePart]: TriangleId}
+
+	-- Spatial hash mapping of verts
+	local mSpatialHash = {} :: {[VertexHash]: VertexId}
+
+	-- Lookup edges by verts
+	local mEdgeLookup = {} :: {[vector]: EdgeId}
+
+	-- TODO: Implement methods
 end
 
 return createTriangleMesh
